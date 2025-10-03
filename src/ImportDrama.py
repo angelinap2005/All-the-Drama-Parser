114af4069288eaf289706b75e8e5d0d9e1653521
Angelina <angelina.p20056@gmail.com>
2025-10-03 13:01:03 +0100

    refactor drama selection to accept user input and validate file extension

--- src/ImportDrama.py
+++ src/ImportDrama.py
@@ -1,21 +1,27 @@
 def select_drama():
-    print("Please select a drama:")
-    print("1. A Midsummer Night's Dream")
-    print("2. Othello the Moor of Venice")
-    print("3. Romeo and Juliet")
-
+    print("Please enter a drama:")
     selection = input()
-    if selection == "1":
-        return "files/A Midsummer Night's Dream.txt"
-    elif selection == "2":
-        return "files/Othello the Moor of Venice.txt"
-    elif selection == "3":
-        return "files/Romeo and Juliet.txt"
+
+    length = len(selection)
+    if length > 0:
+        if selection[:-length] is not ".txt":
+            return None
+        else:
+            return selection
     else:
         return None
 
 def import_drama(drama_file):
     drama = []
+
+    if drama_file is None:
+        return None
+    else:
+        path = "files/" + drama_file
+
+    if path is None:
+        return None
+
     if drama_file is not None:
         with open(drama_file, "r") as file:
             for line in file:
